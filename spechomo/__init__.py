055176fccb49dc3ac44a13a33ece3d51673f540a
Daniel Scheffler <danschef@gfz-potsdam.de>
2019-03-08 20:10:24 +0100

    Added .gitlab_ci.yml

--- spechomo/__init__.py
+++ spechomo/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from .version import __version__, __versionalias__   # noqa (E402 + F401)
+from .prediction import SpectralHomogenizer
 
 
 __author__ = """Daniel Scheffler"""
