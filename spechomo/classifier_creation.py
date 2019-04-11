9e7845a9a5a065569dba7fc79cb4e42c36265173
Daniel Scheffler <danschef@gfz-potsdam.de>
2019-04-12 00:47:49 +0200

    Set max_angle of _extract_best_spectra_from_cluster 5 degrees.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- spechomo/classifier_creation.py
+++ spechomo/classifier_creation.py
@@ -617,7 +617,8 @@ class ClusterClassifier_Generator(object):
         df_src_spectra = df_src_spectra_allclust[df_src_spectra_allclust.cluster_label == clusterlabel]
 
         max_dist = np.percentile(df_src_spectra.spectral_distance, max_distance_percent)
-        max_angle = np.percentile(df_src_spectra.spectral_angle, max_angle_percent)
+        # max_angle = np.percentile(df_src_spectra.spectral_angle, max_angle_percent)
+        max_angle = 5  # degrees  # FIXME hardcoded
         df_src_spectra_best = df_src_spectra[(df_src_spectra.spectral_distance < max_dist) &
                                              (df_src_spectra.spectral_angle < max_angle)]
         df_tgt_spectra_best = df_tgt_spectra_allclust.loc[df_src_spectra_best.index, :]
