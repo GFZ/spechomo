f5139783711c1c6626e58ec30bc7a1f8f3d0214e
Daniel Scheffler <danschef@gfz-potsdam.de>
2019-08-19 13:37:29 +0200

    Updated version info.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- spechomo/version.py
+++ spechomo/version.py
@@ -20,5 +20,5 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.2.3'
-__versionalias__ = '20190813.01'
+__version__ = '0.2.4'
+__versionalias__ = '20190819.01'
