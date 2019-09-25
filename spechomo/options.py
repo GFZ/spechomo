e70e1c5ba65045da3363d4d601609111babf83f0
Daniel Scheffler <danschef@gfz-potsdam.de>
2019-09-25 17:54:19 +0200

    Fixed deprecation warning coming from pyyaml.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- spechomo/options.py
+++ spechomo/options.py
@@ -28,4 +28,4 @@ from spechomo import __path__
 
 
 with open(os.path.join(__path__[0], 'options_default.yaml'), 'r') as stream:
-    options = yaml.load(stream)
+    options = yaml.load(stream, Loader=yaml.BaseLoader)
