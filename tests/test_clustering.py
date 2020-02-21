58c5bba245af67f6b53905a9d8a495c5af5faa17
Daniel Scheffler <danschef@gfz-potsdam.de>
2020-02-21 17:35:57 +0100

    Revised license texts to mention tqdm exceptions.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -12,7 +12,10 @@
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
+# later version. Please note the following exception: `spechomo` depends on tqdm,
+# which is distributed under the Mozilla Public Licence (MPL) v2.0 except for the
+# files "tqdm/_tqdm.py", "setup.py", "README.rst", "MANIFEST.in" and ".gitignore".
+# Details can be found here: https://github.com/tqdm/tqdm/blob/master/LICENCE.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
